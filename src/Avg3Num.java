07ce90c97a831cb0171b61a62bc757386f24b2b9
MihailSHrus <97229510+MihailSHrus@users.noreply.github.com>
2023-06-20 15:09:16 +0400

    Delete some trash

--- src/Avg3Num.java
+++ src/Avg3Num.java
@@ -3,7 +3,6 @@ import java.util.*;
 public class Avg3Num {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String s1 = scanner.toString();
         System.out.println("Введите первое вещестевенное число: ");
         double dnum1 = scanner.nextDouble();
         System.out.println("Вы ввели: " + dnum1);
