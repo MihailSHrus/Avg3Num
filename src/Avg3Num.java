b740e372f76825d29ab3b1c31cced7c97d9d984e
MihailSHrus <97229510+MihailSHrus@users.noreply.github.com>
2023-06-20 13:50:54 +0400

    Initial commit

---
+++ src/Avg3Num.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Avg3Num {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String s1 = scanner.toString();
+        System.out.println("Введите первое вещестевенное число: ");
+        double dnum1 = scanner.nextDouble();
+        System.out.println("Вы ввели: " + dnum1);
+        System.out.println("Введите второе вещестевенное число: ");
+        double dnum2 = scanner.nextDouble();
+        System.out.println("Вы ввели: " + dnum2);
+        System.out.println("Введите третье вещестевенное число: ");
+        double dnum3 = scanner.nextDouble();
+        System.out.println("Вы ввели: " + dnum3);
+        double avg = (dnum1 + dnum2 + dnum3) / 3.0;
+        System.out.print("Среднне арифмитическое введённых чисел - ");
+        System.out.printf("%.4f", avg);
+    }
+}
